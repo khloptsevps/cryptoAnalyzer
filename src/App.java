58563dc527cb2b522658d3152372e7157f9f6081
Petr Khloptsev <p.khloptsev@gmail.com>
2025-04-27 16:16:21 +1000

    add App class

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    private String action;
+    private String pathToFile;
+    private String pathToOutputFile;
+
+    public App(String[] parameters) {}
+
+    public void start() {}
+
+    private void validateAction(String actionName) {}
+
+    private void validatePathToFile(String pathToFile) {}
+}
