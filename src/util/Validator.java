890731d33731d5cd281a6ba52b2bc0d0ee1ca63d
Petr Khloptsev <p.khloptsev@gmail.com>
2025-04-27 21:29:56 +1000

    добавил заготовку для валидатора

---
+++ src/util/Validator.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Validator {
+    public static boolean validateAction(String actionName) {
+        // TODO логику валидации действия
+        return false;
+    }
+    public static boolean validateFile(String pathToFile) {
+        // TODO логику валидации файла, существует ли файл по переданному пути
+        return false;
+    }
+    public static boolean validatePath(String pathToFile) {
+        // TODO логику валидации пути, проверить корректен ли путь.
+        return false;
+    }
+}
