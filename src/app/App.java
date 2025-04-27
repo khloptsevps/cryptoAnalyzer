8f064e95f4e2724ea68198552dc0bc05f152fecd
Petr Khloptsev <p.khloptsev@gmail.com>
2025-04-27 21:29:26 +1000

    добавил класс App

---
+++ src/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+public class App {
+    private String action;
+    private String PathToFile;
+    private String PathToOutputFile;
+    private String key;
+
+    App(String[] parameters) {
+
+    }
+
+    public void start() {
+        System.out.println("App started");
+    }
+}
