1293fd44b002a9df4d2bb3576b82c66bb55cb1d2
Petr Khloptsev <p.khloptsev@gmail.com>
2025-04-29 10:51:02 +1000

    добавлена валидация запрашиваемого действия

--- src/util/Validator.java
+++ src/util/Validator.java
@@ -1,9 +1,17 @@
 package util;
+import actions.Actions;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Validator {
+    private static final Set<String> ACTIONS = new HashSet<>();
+    static {
+        for (Actions action: Actions.values()) {
+            ACTIONS.add(action.name().toLowerCase());
+        }
+    }
     public static boolean validateAction(String actionName) {
-        // TODO логику валидации действия
-        return false;
+        return ACTIONS.contains(actionName.toLowerCase());
     }
     public static boolean validateFile(String pathToFile) {
         // TODO логику валидации файла, существует ли файл по переданному пути
