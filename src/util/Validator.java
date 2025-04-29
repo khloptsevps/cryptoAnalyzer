c8c3e56e628a26dbdb734161f00403ddecc56580
Petr Khloptsev <p.khloptsev@gmail.com>
2025-04-29 11:00:50 +1000

    переименован метод валидации действия

--- src/util/Validator.java
+++ src/util/Validator.java
@@ -5,14 +5,17 @@ import java.util.Set;
 
 public class Validator {
     private static final Set<String> ACTIONS = new HashSet<>();
+
     static {
         for (Actions action: Actions.values()) {
             ACTIONS.add(action.name().toLowerCase());
         }
     }
-    public static boolean validateAction(String actionName) {
+
+    public static boolean isValidAction(String actionName) {
         return ACTIONS.contains(actionName.toLowerCase());
     }
+
     public static boolean validateFile(String pathToFile) {
         // TODO логику валидации файла, существует ли файл по переданному пути
         return false;
