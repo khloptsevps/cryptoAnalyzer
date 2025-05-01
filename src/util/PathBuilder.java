115c2acb09c2beaf11077aba321870b5c9a1e572
Petr Khloptsev <p.khloptsev@gmail.com>
2025-05-01 14:37:47 +1000

    Добавлен метод getDefaultPath. Удалены ненужные методы.

--- src/util/PathBuilder.java
+++ src/util/PathBuilder.java
@@ -2,35 +2,24 @@ package util;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
 
 public class PathBuilder {
     private final String DEFAULT_PATH = System.getProperty("user.dir");
-    private final List<Path> pathList = new ArrayList<>();
-    private final Set<Path> pathSet = new HashSet<>();
-//    private final HashMap<String, Path> inOutPath = new HashMap<>();
+    private final String DEFAULT_DIR = "test";
 
-//    public void buildInputAndOutputPath(String inputPath, String outputPath) {
-//        Path input = buildPath(inputPath);
-//        Path output = buildPath(outputPath);
-//        inOutPath.put("input", input);
-//        inOutPath.put("output", output);
-//    }
-
-    public void addPath(String path) {
-        Path newPath = buildPath(path);
-        if (!pathSet.contains(newPath)) {
-            pathList.add(newPath);
-            pathSet.add(newPath);
-        }
+    public Path getPath(String path) {
+        return buildPath(path);
     }
 
-    public List<Path> getPathList() {
-        return pathList;
+    public Path getDefaultPath() {
+        return Paths.get(DEFAULT_PATH, DEFAULT_DIR);
+    }
+
+    public Path getDefaultPath(String fileName) {
+        return Paths.get(DEFAULT_PATH, DEFAULT_DIR, fileName);
     }
 
     private Path buildPath(String path) {
-        final String DEFAULT_DIR = "test";
         Path currentPath = Paths.get(path);
         if (!currentPath.isAbsolute()) {
             if (currentPath.getParent() == null) {
@@ -39,7 +28,7 @@ public class PathBuilder {
                 currentPath = Paths.get(DEFAULT_PATH, path);
             }
         }
-        return currentPath;
+        return currentPath.normalize();
     }
 
 
