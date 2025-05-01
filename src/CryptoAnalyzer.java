0e0b2cf48445251afe3433b4c5d950f74572bdba
Petr Khloptsev <p.khloptsev@gmail.com>
2025-05-01 14:44:07 +1000

    Обновил заметки. Обновлена логика main()

--- src/CryptoAnalyzer.java
+++ src/CryptoAnalyzer.java
@@ -4,9 +4,9 @@ import appView.View;
 
 public class CryptoAnalyzer {
     public static void main(String[] args) {
-        // если параметров нет, то запускаем консольный режим
+        // Если параметров нет, то запускаем консольный режим
         // args из командной строки действие, путь до файла, путь выходного файла, ключ.
-        // установка состояния приложения в контроллере.
+        // Установка состояния приложения в контроллере.
 
         View ui = new ConsoleView();
         ConsoleController controller = new ConsoleController(ui);
