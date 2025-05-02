d204d1678b64a05d769529ece29cc6d425d009f8
Petr Khloptsev <p.khloptsev@gmail.com>
2025-05-02 16:11:52 +1000

    DTO как record

--- src/model/dto/CipherRequest.java
+++ src/model/dto/CipherRequest.java
@@ -2,32 +2,5 @@ package model.dto;
 
 import java.nio.file.Path;
 
-public class CipherRequest {
-    private final String action;
-    private final Path inputPath;
-    private final Path outputPath;
-    private final int key;
-
-    public CipherRequest(String action, Path inputPath, Path outputPath, int key) {
-        this.action = action;
-        this.inputPath = inputPath;
-        this.outputPath = outputPath;
-        this.key = key;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    public Path getInputPath() {
-        return inputPath;
-    }
-
-    public Path getOutputPath() {
-        return outputPath;
-    }
-
-    public int getKey() {
-        return key;
-    }
+public record CipherRequest(String action, Path inputPath, Path outputPath, int key) {
 }
