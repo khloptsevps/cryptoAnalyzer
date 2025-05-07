4bda69bae0c6e3dbb537f45fc2e69a160df7bcd4
Petr Khloptsev <p.khloptsev@gmail.com>
2025-05-07 13:17:48 +1000

    Добавлен метод для получения действия

--- src/model/actions/Actions.java
+++ src/model/actions/Actions.java
@@ -1,17 +1,27 @@
 package model.actions;
 
 public enum Actions {
-    ENCRYPT("\uD83D\uDD10 Зашифровать"),
-    DECRYPT("\uD83D\uDD13 Расшифровать"),
-    EXIT("\uD83D\uDC4B Завершить работу приложения");
+    ENCRYPT("\uD83D\uDD10 Зашифровать", new Encrypt()),
+    DECRYPT("\uD83D\uDD13 Расшифровать", new Decrypt()),
+    EXIT("\uD83D\uDC4B Завершить работу приложения", new Exit());
 
     private final String label;
+    private final Action action;
 
-    Actions(String label) {
+    Actions(String label, Action action) {
         this.label = label;
+        this.action = action;
     }
 
     public String getLabel() {
         return this.label;
+    }
+
+    public Action getAction() {
+        return this.action;
+    }
+
+    public static Action getActionByName(String actionName) {
+          return Actions.valueOf(actionName.toUpperCase()).getAction();
     }
 }
