9815a9d45fc74444b7fdaa533f9cbf503b5e48a0
Petr Khloptsev <p.khloptsev@gmail.com>
2025-05-12 17:23:12 +1000

    реализован readChunk для чтения данных частями

--- src/model/CaesarCipherModel.java
+++ src/model/CaesarCipherModel.java
@@ -1,14 +1,48 @@
 package model;
 
 import model.alphabet.Alphabet;
+import model.alphabet.Alphabets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 public class CaesarCipherModel {
-    private final String abTest = "абвгдеёжзий";
+    private BufferedReader reader;
 
-    Alphabet alphabet = new Alphabet(abTest);
+    Alphabet alphabet = new Alphabet(Alphabets.getRussianAlphabet());
+
+    public Alphabet getAlphabet() {
+        return alphabet;
+    }
 
     public int getAlphabetSize() {
         return alphabet.getSize();
     }
 
+
+    public void openStreams(Path pathToFile) throws IOException {
+        reader = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8);
+    }
+
+    public void closeStreams() throws IOException {
+        if (reader != null) {
+            reader.close();
+        }
+    }
+
+    public char[] readChunk(int chunkSize) throws IOException {
+        char[] buffer = new char[chunkSize];
+        int charsRead = reader.read(buffer);
+        if (charsRead == -1) {
+            return null;
+        }
+        if (charsRead < chunkSize) {
+            return Arrays.copyOf(buffer, charsRead);
+        }
+        return buffer;
+    }
 }
