38356f40a8b1d0d31646fb1eb37b421edad938f7
Petr Khloptsev <p.khloptsev@gmail.com>
2025-05-12 17:10:14 +1000

    реализовано шифрование с ключем.

--- src/model/actions/Encrypt.java
+++ src/model/actions/Encrypt.java
@@ -1,11 +1,40 @@
 package model.actions;
 
 import model.CaesarCipherModel;
+import model.alphabet.Alphabet;
 import model.context.CipherContext;
+import java.io.IOException;
+
 
 public class Encrypt implements Action {
     @Override
     public void execute(CipherContext context) {
+        CaesarCipherModel cipherModel = context.getCipherModel();
+        Alphabet alphabet = cipherModel.getAlphabet();
+
+        try {
+            cipherModel.openStreams(context.getInputPath());
+            char[] currentChars = cipherModel.readChunk(context.getChunkSize());
+            do {
+                char[] encrypted = new char[currentChars.length];
+                for (int i = 0; i < currentChars.length; i++) {
+                    char currentChar = currentChars[i];
+                    boolean isUpperCase = Character.isUpperCase(currentChar);
+                    char newChar = alphabet.shift(currentChar, context.getShiftKey());
+                    encrypted[i] = isUpperCase ? Character.toUpperCase(newChar) : newChar;
+                }
+
+                currentChars = cipherModel.readChunk(context.getChunkSize());
+            } while (currentChars != null);
+
+            cipherModel.closeStreams();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
     }
 }
 
