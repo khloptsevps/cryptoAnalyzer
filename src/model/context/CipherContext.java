6c06d590cdc540eaa5799766f89070db9edf3ade
Petr Khloptsev <p.khloptsev@gmail.com>
2025-05-12 13:21:44 +1000

    добавлен контекст приложения

---
+++ src/model/context/CipherContext.java
@@ -0,0 +1,35 @@
+package model.context;
+
+import model.CaesarCipherModel;
+
+import java.nio.file.Path;
+
+public class CipherContext {
+    final CaesarCipherModel cipherModel;
+    final Path inputPath;
+    final Path outputPath;
+    final int shiftKey;
+
+    public CipherContext(CaesarCipherModel cipherModel, Path inputPath, Path outputPath, int shiftKey) {
+        this.cipherModel = cipherModel;
+        this.inputPath = inputPath;
+        this.outputPath = outputPath;
+        this.shiftKey = shiftKey;
+    }
+
+    public CaesarCipherModel getCipherModel() {
+        return cipherModel;
+    }
+
+    public Path getInputPath() {
+        return inputPath;
+    }
+
+    public Path getOutputPath() {
+        return outputPath;
+    }
+
+    public int getShiftKey() {
+        return shiftKey;
+    }
+}
