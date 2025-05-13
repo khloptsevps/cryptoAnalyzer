89d05dc0e091184b245b16c9310f90154b820fa6
Petr Khloptsev <p.khloptsev@gmail.com>
2025-05-13 11:12:24 +1000

    добавлен метод шифрования и расшифровки

--- src/model/CaesarCipherModel.java
+++ src/model/CaesarCipherModel.java
@@ -3,46 +3,25 @@ package model;
 import model.alphabet.Alphabet;
 import model.alphabet.Alphabets;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-
 public class CaesarCipherModel {
-    private BufferedReader reader;
 
     Alphabet alphabet = new Alphabet(Alphabets.getRussianAlphabet());
-
-    public Alphabet getAlphabet() {
-        return alphabet;
-    }
 
     public int getAlphabetSize() {
         return alphabet.getSize();
     }
 
-
-    public void openStreams(Path pathToFile) throws IOException {
-        reader = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8);
+    public char[] encrypt(char[] chars, int shiftKey) {
+        char[] result = new char[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            char currentChar = chars[i];
+            char shiftedChar = alphabet.shift(currentChar, shiftKey);
+            result[i] = shiftedChar;
+        }
+        return result;
     }
 
-    public void closeStreams() throws IOException {
-        if (reader != null) {
-            reader.close();
-        }
-    }
-
-    public char[] readChunk(int chunkSize) throws IOException {
-        char[] buffer = new char[chunkSize];
-        int charsRead = reader.read(buffer);
-        if (charsRead == -1) {
-            return null;
-        }
-        if (charsRead < chunkSize) {
-            return Arrays.copyOf(buffer, charsRead);
-        }
-        return buffer;
+    public char[] decrypt(char[] chars, int shiftKey) {
+        return encrypt(chars, -shiftKey);
     }
 }
