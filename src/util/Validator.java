ed222691605f4b6c636b1812928ff1cfd372d46a
Petr Khloptsev <p.khloptsev@gmail.com>
2025-06-07 00:46:11 +1000

    добавлена валидация контекста и одинаковых путей

--- src/util/Validator.java
+++ src/util/Validator.java
@@ -1,5 +1,7 @@
 package util;
 
+import model.context.CipherContext;
+
 import java.nio.file.*;
 
 public class Validator {
@@ -14,5 +16,13 @@ public class Validator {
 
     public static boolean isProbablyDirectory(Path path) {
         return !path.getFileName().toString().contains(".");
+    }
+
+    public static boolean isSamePath(Path path1, Path path2) {
+        return path1.equals(path2);
+    }
+
+    public static boolean validateContext(CipherContext context) {
+        return context.getShiftKey() != -1 && context.getInputPath() != null && context.getOutputPath() != null;
     }
 }
