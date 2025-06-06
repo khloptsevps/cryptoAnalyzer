d57aaca41c22958caea230c918e00ec12a077c1c
Petr Khloptsev <p.khloptsev@gmail.com>
2025-06-07 00:48:01 +1000

    добавлена панель выбора файлов

---
+++ src/appView/SwingUI/panels/FileChooserPanel.java
@@ -0,0 +1,30 @@
+package appView.SwingUI.panels;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Map;
+
+public class FileChooserPanel extends JPanel implements Panel {
+    private JPanel jPanel = new JPanel();
+    private final Map<String, JButton> IO_BUTTONS;
+
+    public FileChooserPanel(Map<String, JButton> chooserButtons) {
+        IO_BUTTONS = chooserButtons;
+        init();
+    }
+
+
+    private void init() {
+        jPanel.setBorder(BorderFactory.createTitledBorder("Выберите файлы"));
+        jPanel.setLayout(new GridLayout(2, 1, 10, 20));
+
+        IO_BUTTONS.forEach( (_, button) -> {
+            jPanel.add(button);
+        });
+    }
+
+    @Override
+    public JPanel getPanel() {
+        return jPanel;
+    }
+}
