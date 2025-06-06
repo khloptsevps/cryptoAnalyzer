ce60f275b5756b9af14b0d60d7d52b36b9a72143
Petr Khloptsev <p.khloptsev@gmail.com>
2025-06-07 00:54:13 +1000

    добавлен графический интерфейс

--- src/CryptoAnalyzer.java
+++ src/CryptoAnalyzer.java
@@ -1,16 +1,27 @@
 import appController.Controller;
+import appController.SwingController;
 import appView.ConsoleView;
+import appView.SwingUI.SwingView;
 import appView.View;
 import model.CaesarCipherModel;
 
+import javax.swing.*;
+
 public class CryptoAnalyzer {
     public static void main(String[] args) {
-
-        View ui = new ConsoleView();
-        CaesarCipherModel model = new CaesarCipherModel();
-        Controller controller = new Controller(ui, model);
-
-        controller.run();
-
+        if (args.length > 0 && args[0].equals("-gui")) {
+            SwingUtilities.invokeLater(() -> {
+                CaesarCipherModel model = new CaesarCipherModel();
+                SwingView ui = new SwingView();
+                SwingController controller = new SwingController(ui, model);
+                controller.initApp();
+                controller.run();
+            });
+        } else {
+            View ui = new ConsoleView();
+            CaesarCipherModel model = new CaesarCipherModel();
+            Controller controller = new Controller(ui, model);
+            controller.run();
+        }
     }
 }
